81347e2e3fd7357f0d5d2db1f54ce780e9485d23
Mateus Rocha <135074699+Mateus21022@users.noreply.github.com>
2025-02-14 15:02:08 -0300

    Add files via upload

---
+++ main (1).r
@@ -0,0 +1,54 @@
+#Mateus de Jesus Rocha e Eduardo Sousa Passos
+
+#Questão 1.
+x <- 15:50 
+
+y <- 10:5
+print (y)
+print (x)
+#--------------------------------------------------------------------#
+
+#Questão 2. 
+x<- c("a",4,10.5,NaN)
+#is.numeric(x)
+#is.logical(x)
+#is.character(x)
+#is.complex(x)
+#is.integer(x)
+
+#--------------------------------------------------------------------#
+
+#Questão 3.
+#matriz_1 <- matrix(data = 1:20, nrow = 4, ncol = 5) 
+#print (matriz_1)
+#dim (matriz_1)
+
+#--------------------------------------------------------------------#
+
+#Questão 4. 
+#a <- 1:25
+#dim(a)<-c(5,5)
+#print (a)
+
+#--------------------------------------------------------------------#
+
+#Questão 5.
+
+#x <- seq ( from = 1, to = 5, by = 1)
+#print (x)
+#y <- seq ( from = 5, to = 25, by = 5)
+#print (y)
+
+#Combinar por Linhas
+#rbind(x, y)
+
+#Combinar por Colunas:
+#cbind (x, y)
+
+#--------------------------------------------------------------------#
+
+#Questão 6.
+
+#x <- 1:5
+#names(x)<-c("New York", "Seattle", "Los Angeles", "Chicago", "Houston")
+#x
